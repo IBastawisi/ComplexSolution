c5f09d065b9a2fd5ae028442e752c228a7a42dc2
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-03-15 00:05:03 +0200

    Add complex class library and console app

---
+++ ComplexConsole/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+using ComplexLibrary;
+
+static ComplexNumber ReadComplexNumber()
+{
+  WriteLine("Enter a complex number in the form a + bi: ");
+  string? input = ReadLine();
+  if (input is null)
+  {
+    WriteLine("Invalid input");
+    return ReadComplexNumber();
+  }
+  ComplexNumber? result = ComplexNumber.TryParse(input);
+  if (result is null)
+  {
+    WriteLine("Invalid input");
+    return ReadComplexNumber();
+  }
+  return result;
+}
+
+static Operation ReadOperation()
+{
+  WriteLine("Enter an operation (+, -, *, /): ");
+  string? input = ReadLine();
+  if (input is null)
+  {
+    WriteLine("Invalid input");
+    return Operation.Add;
+  }
+  return input switch
+  {
+    "+" => Operation.Add,
+    "-" => Operation.Subtract,
+    "*" => Operation.Multiply,
+    "/" => Operation.Divide,
+    _ => Operation.Add
+  };
+}
+
+static void Calculate ()
+{
+  var a = ReadComplexNumber();
+  var b = ReadComplexNumber();
+  var operation = ReadOperation();
+
+  var result = operation switch
+  {
+    Operation.Add => a + b,
+    Operation.Subtract => a - b,
+    Operation.Multiply => a * b,
+    Operation.Divide => a / b,
+    _ => a + b
+  };
+
+  WriteLine($"{a} {operation} {b} = {result}");
+}
+
+while (true)
+{
+  Calculate();
+}
+
+enum Operation
+{
+  Add,
+  Subtract,
+  Multiply,
+  Divide
+}
+
---
+++ ComplexLibrary/ComplexNumber.cs
@@ -0,0 +1,66 @@
+﻿namespace ComplexLibrary;
+public class ComplexNumber
+{
+  public double Real { get; set; }
+  public double Imaginary { get; set; }
+  public ComplexNumber(double real, double imaginary)
+  {
+    Real = real;
+    Imaginary = imaginary;
+  }
+  public override string ToString()
+  {
+    return $"{Real} + {Imaginary}i";
+  }
+
+  public static ComplexNumber? TryParse(string s)
+  {
+    if (String.IsNullOrEmpty(s))
+    {
+      return null;
+    }
+
+    string[] parts = s.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+    if (parts.Length != 2)
+    {
+      return null;
+    }
+    if (!double.TryParse(parts[0], out double real))
+    {
+      return null;
+    }
+    if (!double.TryParse(parts[1].TrimEnd('i'), out double imaginary))
+    {
+      return null;
+    }
+
+    if (s.Contains('-'))
+    {
+      imaginary = -imaginary;
+    }
+
+    return new ComplexNumber(real, imaginary);
+  }
+
+  public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+  {
+    return new ComplexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
+  }
+
+  public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+  {
+    return new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
+  }
+
+  public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+  {
+    return new ComplexNumber(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Real * b.Imaginary + a.Imaginary * b.Real);
+  }
+
+  public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+  {
+    double denominator = b.Real * b.Real + b.Imaginary * b.Imaginary;
+    return new ComplexNumber((a.Real * b.Real + a.Imaginary * b.Imaginary) / denominator, (a.Imaginary * b.Real - a.Real * b.Imaginary) / denominator);
+  }
+}
