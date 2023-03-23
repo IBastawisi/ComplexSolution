be13e56e84b909ce4c1e2a3fe66bf0c86b81badf
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-03-23 17:31:05 +0200

    Add API endpoint documentation

--- ComplexApi/Controllers/CalculatorController.cs
+++ ComplexApi/Controllers/CalculatorController.cs
@@ -5,13 +5,29 @@ namespace ComplexApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+
 public class CalculatorController : ControllerBase
 {
-    public CalculatorController() { }
-    [HttpGet(Name = "Calculate")]
-    public string Calculate(string a, string b, string operation)
+  /// <summary>
+  /// Calculates the result of a complex number operation
+  /// </summary>
+  /// <param name="a">The first complex number in the form a+bi</param>
+  /// <param name="b">The second complex number in the form a+bi</param>
+  /// <param name="operation">The operation to perform (+, -, *, /) </param>
+  /// <returns>The result of the operation</returns>
+  /// <response code="200">Returns the result of the operation</response>
+  /// <response code="400">If the input is invalid</response>
+  [HttpGet(Name = "Calculate")]
+  public string Calculate(string a, string b, string operation)
+  {
+    try
     {
-        var result = ComplexCalculator.Calculate(a, b, operation).ToString();
-        return result;
+      return ComplexCalculator.Calculate(a, b, operation).ToString();
     }
+    catch (Exception e)
+    {
+      Response.StatusCode = 400;
+      return e.Message;
+    }
+  }
 }
